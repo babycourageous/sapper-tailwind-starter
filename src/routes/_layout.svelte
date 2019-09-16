1b030d7dec5528c13036c4f6ee7da7cb0f9d9d87
Rene Dellefont <renedellefont@gmail.com>
2019-09-16 19:45:40 -0400

    Add Tailwind to layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,18 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from '../components/Nav.svelte'
 
-	export let segment;
+  export let segment
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+<style type="text/postcss">
+  main {
+    @apply relative bg-pink-400;
+    max-width: 56em;
+  }
 </style>
 
-<Nav {segment}/>
+<Nav {segment} />
 
-<main>
-	<slot></slot>
+<main class="my-0 mx-auto p-8">
+  <slot />
 </main>
